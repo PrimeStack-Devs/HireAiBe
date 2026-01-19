266fa309bf02ab8d461616fbe19edd7ea013140c
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2026-01-20 00:41:03 +0530

    refactor: comment out SQLite configuration and retain PostgreSQL settings

--- config/database.js
+++ config/database.js
@@ -2,23 +2,23 @@ const path = require("path");
 
 module.exports = ({ env }) => {
   return {
-          connection: {
-        client: "sqlite",
-        connection: {
-          filename: path.join(__dirname, "..", env("DATABASE_FILENAME", ".tmp/data.db")),
-        },
-        useNullAsDefault: true,
+      //     connection: {
+      //   client: "sqlite",
+      //   connection: {
+      //     filename: path.join(__dirname, "..", env("DATABASE_FILENAME", ".tmp/data.db")),
+      //   },
+      //   useNullAsDefault: true,
+      // },
+    connection: {
+      client: "postgres",
+      connection: {
+        host: env("PGHOST", "127.0.0.1"),
+        port: env.int("PGPORT", 24569),
+        database: env("PGDATABASE", "strapi"),
+        user: env("PGUSER", "strapi"),
+        password: env("PGPASSWORD", "password"),
+        // ssl: true,
       },
-    // connection: {
-    //   client: "postgres",
-    //   connection: {
-    //     host: env("PGHOST", "127.0.0.1"),
-    //     port: env.int("PGPORT", 24569),
-    //     database: env("PGDATABASE", "strapi"),
-    //     user: env("PGUSER", "strapi"),
-    //     password: env("PGPASSWORD", "password"),
-    //     // ssl: true,
-    //   },
-    // },
+    },
   };
 };
